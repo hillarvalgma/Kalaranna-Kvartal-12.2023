c73e306d4f0c7a37fe2f2ed339c1f6c73d60848f
hillarvalgma <74972103+hillarvalgma@users.noreply.github.com>
2023-12-08 14:08:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,7 @@
       { cubeMapPreviewUrl: urlPrefix + "/" + data.id + "/preview.jpg" });
     var geometry = new Marzipano.CubeGeometry(data.levels);
 
-    var limiter = Marzipano.RectilinearView.limit.traditional(2 * Data.faceSize, 100*Math.PI/180, 120*Math.PI/180);
+    var limiter = Marzipano.util.compose(Marzipano.RectilinearView.limit.vfov(0.698131111111111, 2.09439333333333), Marzipano.RectilinearView.limit.hfov(0.698131111111111, 2.09439333333333), Marzipano.RectilinearView.limit.pitch(-Math.PI/2, Math.PI/2));
     var view = new Marzipano.RectilinearView(data.initialViewParameters, limiter);
 
     var scene = viewer.createScene({
